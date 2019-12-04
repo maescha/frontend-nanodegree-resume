30629abccfbf471ecae89d72bba8c4a598a20a45
maescha <40622257+maescha@users.noreply.github.com>
2019-12-04 16:54:17 -0700

    adding more objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,20 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+    "name": "Josephine Su",
+    "role": "shrug",
+    "age": 22,
+    "contacts": {
+       "mobile": "123-123-1234",
+       "email": "hi@hi.com",
+       "github": "to be filled",
+       "location": "edmonton"
+    },
+    "bioPic": "images\ApplicationFrameHost_2019-12-04_16-40-15.png",
+    "welcomeMessage": "hello",
+    "skills": ["writing, ", "learning ", "..."]
+
+}
+
+$("#main").append(bio);
