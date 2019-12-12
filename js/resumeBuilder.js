e1c7374b61b2626519ecd18a998aa8be77454ff6
maescha <40622257+maescha@users.noreply.github.com>
2019-12-12 16:44:14 -0700

    for in loop save
    -- copy and pasted

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,28 +1,76 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
 var bio = {
     name: "Josephine Su",
     role: "shrug",
-    age: 22,
     contacts: {
        mobile: "123-123-1234",
        email: "hi@hi.com",
        github: "to be filled",
+       twitter: "@twitter",
        location: "edmonton"
     },
-    bioPic: "images\ApplicationFrameHost_2019-12-04_16-40-15.png",
+    bioPic: "images/fry.jpg",
     welcomeMessage: "hello",
     skills: ["writing, ", "learning ", "..."],
 }
 
-//???? just copied from the video this format....
-// var work = {};
-//     work.employer = "HM";
-//     work.title: "sales advisor";
-//     work.location: "WEM";
-//     work.dates: "09/19 - present ";
-//     work.description: "minimal wage retail job yall";
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+//var formattedContacts = HTMLcontactGeneric.replace("%data%", bio.contacts);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+var formattedSkillsStart = HTMLskillsStart.replace("%data%", bio.contacts);
+
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
+$("#header").append(formattedMobile);
+$("#header").append(formattedEmail);
+$("#header").append(formattedTwitter);
+$("#header").append(formattedGithub);
+$("#header").append(formattedLocation);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMessage);
+$("#header").append(formattedSkills);
+
+
+if (bio.skills < 0){ //checks if there is an onlinecourse in education object
+    
+    $("#header").append(HTMLskillsStart)
+    
+    var formattedSkills = HTMLskills.replace("%data%", bio.skills[0]);
+    $("#skills").append(formattedSkills);
+    formattedSkills = HTMLskills.replace("%data%", bio.skills[1]);
+    $("#skills").append(formattedSkills);
+}
+
+
+var work = {
+    jobs: { 
+        employer: "HM",
+        title: "what else ;-;",
+        location: "WEM",
+        dates: "09/19 - present",
+        description: "minimal wage retail job yall",
+    }, 
+}
+
+var projects = {
+    projects: {
+        title: "Front-end Nanodegree Resume",
+        dates: "shrug - present",
+        description: " ",
+        images: {
+            img1: "img link here",
+        },
+    },
+}
 
 var education = {
     schools: {
@@ -33,17 +81,25 @@ var education = {
         dates: "September 2015 - June 2019",
     },
     OnlineCourses: {
-    title: "Front-end Web Developer Nanodegree",
-    school: "Udacity",
-    dates: "August 2019 - in progress",
-    url: "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011"
+        title: "Front-end Web Developer Nanodegree",
+        school: "Udacity",
+        dates: "August 2019 - in progress",
+        url: "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011"
 },
 }
 
+for (job in work.jobs) {
+    $("#workExperience").append(HTMLworkStart);
+    //concatenate employer and title
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs.employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs.title);
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle);
+    //dates
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs.dates);
+    $(".work-entry:last").append(formattedDates);
+    //descript
+    var formattedDescript = HTMLworkDescription.replace("%data%", work.jobs.description);
+    $(".work-entry:last").append(formattedDescript);
 
-$("#main").prepend(bio.name);
-
-//i dont knowwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-
-$("#main").append(work["employer"]);
-$("#main").append(education.name);
+}
