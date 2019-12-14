db3919fbe95cc13196d5ca3d9629a53a9328b947
maescha <40622257+maescha@users.noreply.github.com>
2019-12-13 21:15:11 -0700

    made contacts all into one line
    and broke welcomeMessage off, fixed arrays, hid internalizeButton, added functions, and more stuff

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,7 +2,7 @@
 var bio = {
     name: "Josephine Su",
     role: "shrug",
-    contacts: {
+    contacts: { 
        mobile: "123-123-1234",
        email: "hi@hi.com",
        github: "to be filled",
@@ -11,7 +11,7 @@ var bio = {
     },
     bioPic: "images/fry.jpg",
     welcomeMessage: "hello",
-    skills: ["writing, ", "learning ", "..."],
+    skills: ["writing ", " learning ", " ..."],
 }
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
@@ -40,7 +40,7 @@ $("#header").append(formattedWelcomeMessage);
 $("#header").append(formattedSkills);
 
 
-if (bio.skills < 0){ //checks if there is an onlinecourse in education object
+if (bio.skills < 0){ //checks if there are skills in my bio
     
     $("#header").append(HTMLskillsStart)
     
@@ -52,54 +52,111 @@ if (bio.skills < 0){ //checks if there is an onlinecourse in education object
 
 
 var work = {
-    jobs: { 
-        employer: "HM",
-        title: "what else ;-;",
-        location: "WEM",
-        dates: "09/19 - present",
-        description: "minimal wage retail job yall",
-    }, 
+    jobs: [     
+        {       //THIS IS INSIDE THE JOBS ARRAY IN THE WORK OBJECT
+            employer: "HM",
+            title: "what else ;-;",
+            location: "WEM",
+            dates: "09/19 - present",
+            description: "minimal wage retail job yall",
+        }
+    ], 
 }
 
 var projects = {
-    projects: {
-        title: "Front-end Nanodegree Resume",
-        dates: "shrug - present",
-        description: " ",
-        images: {
-            img1: "img link here",
-        },
-    },
+    projects: [
+        {
+            title: "Front-end Nanodegree Resume",
+            dates: "shrug - present",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget finibus neque, nec rhoncus lectus. Duis fermentum, dolor sed ornare gravida, magna sem maximus justo, et luctus est eros dapibus nunc. Proin tristique semper orci id placerat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin a auctor odio, sit amet rutrum diam. In finibus, ipsum elementum consectetur auctor, lorem diam vestibulum nibh, interdum vulputate nibh sem id mauris. Suspendisse quis leo ut ante volutpat rhoncus nec eu enim. Pellentesque tempor vitae massa non posuere. Cras molestie justo nec sollicitudin pulvinar. Cras convallis leo ac nunc euismod sodales.",
+            images: {
+                img1: "img link here",
+            }
+        }
+    ]
 }
 
 var education = {
-    schools: {
+    schools: [
+        {
         name: "University of Alberta",
         location: "Edmonton, Alberta",
         degree: "Bachelor of Arts",
         specialization: "Film Studies, minor in Comparative Literature",
         dates: "September 2015 - June 2019",
-    },
-    OnlineCourses: {
+        }
+    ]
+    ,
+    OnlineCourses: [
+        {
         title: "Front-end Web Developer Nanodegree",
         school: "Udacity",
         dates: "August 2019 - in progress",
         url: "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011"
-},
+        }
+    ]
 }
 
-for (job in work.jobs) {
-    $("#workExperience").append(HTMLworkStart);
-    //concatenate employer and title
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs.employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs.title);
-    var formattedEmployerTitle = formattedEmployer + formattedTitle;
-    $(".work-entry:last").append(formattedEmployerTitle);
-    //dates
-    var formattedDates = HTMLworkDates.replace("%data%", work.jobs.dates);
-    $(".work-entry:last").append(formattedDates);
-    //descript
-    var formattedDescript = HTMLworkDescription.replace("%data%", work.jobs.description);
-    $(".work-entry:last").append(formattedDescript);
-
+function displayWork() {
+    for (job in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
+        //concatenate employer and title
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
+        //dates
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        $(".work-entry:last").append(formattedDates);
+        //descript
+        var formattedDescript = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(formattedDescript);
+    }
 }
+
+displayWork(); 
+
+//logs where you click on the resume page
+$(document).click(function(loc) {
+    var x = loc.pageX;
+    var y = loc.pageY; 
+    logClicks(x,y);
+  });
+
+//hiding internationalize button that doesnt even work lol
+// function inName(name) {
+//     name = name.trim().split(" ");
+//     console.log(name);
+//     name[1] = name[1].toUpperCase();
+//     name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).ToLowerCase();
+//     return name[0] +" "+ name[1];
+// }
+//$('#main').append(internationalizeButton);
+
+projects.display = function() {
+    for (project in projects.projects) {
+        $("#projects").append(HTMLprojectStart);
+        //getting project title
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        $(".project-entry:last").append(formattedTitle)
+        //getting project date
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        $(".project-entry:last").append(formattedDates)
+        //getting description
+        var formattedDescript = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+        $(".project-entry:last").append(formattedDescript)
+
+        //appending images
+        if (projects.projects[project].images.length > 0) {
+            for (image in projects.projects[project].images){
+                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+                $(".project-entry:last").append(formattedImage);
+            }
+        }
+    }
+}
+
+projects.display();
+
+// NOT GONNA DO THE GOOGLE MAP BECAUSE REQUIRES MONEY
+// $("#mapDiv").append(googleMap);
